85e3aad2eb438f5e54619566df055e9dfb32d044
Manilamagar <manilamagar200@gmail.com>
2025-12-08 16:10:19 +0545

    make change

---
+++ .history/main_20251208160814.dart
@@ -0,0 +1,60 @@
+void main() {
+  // print("dart programming");
+
+  // int num = 1;
+  // double price = 2.0;
+
+  // String name = "bit";
+  // bool isBit = true;
+  // //final, const,var,dynamic
+  // print(num);
+  // print(price);
+  // //print(name);
+  // print(isBit);
+
+  // var names = "text";
+  // print(names);
+
+  // List<String> name = ["1", "2", "3"];
+
+  // List n = [1, 2, 3];
+  // final reversed = n.reversed;
+  // print(reversed);
+
+  // name[0];
+  // print("${name[2]}");
+
+  // name.add("value");
+  // name.addAll(["iterable", ""]);
+
+  //Map
+  //{
+  // "h":"ff"
+  // }
+
+  //Map -->key value pair
+  //String,string
+
+  // Map names = {"fullname": 1};
+  // final value = names["fullname"];
+
+  // final newvalue = names['fullname'] = 2; //assign new value or update value
+  // print(value);
+
+  // print(newvalue);
+
+  Person name = Person("name", "address");
+  print(name.name);
+  print(name.address);
+  print(name.home);
+}
+
+//positional
+class Person {
+  final String name;
+  final String address;
+  final String? home; //optional //nullable ?
+  Person(this.name, this.address, [this.home]);
+}
+
+
